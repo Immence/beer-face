bbc3c73979408c6c1a69c4e6b8051e213a3230c7
halvard <hal.egg@gmail.com>
2017-06-03 01:29:24 +0200

    Removede debug code :)

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,9 +1,6 @@
 import restify from 'restify';
-import fs from 'fs';
-import path from 'path';
 import { getBeer, listBeers, addBeer } from '../controllers/beers';
 import { listImages, storeImage, getFaces, suggestBeers, getS3UploadParams } from '../controllers/suggestions';
-
 
 export default class PublicRoutes {
 
@@ -12,22 +9,13 @@ export default class PublicRoutes {
   }
 
   setupRoutes() {
-    this.server.get('/', () => {
-      const testFolder = __dirname;
-      fs.readdir(testFolder, (err, files) => {
-        files.forEach((file) => {
-          console.log(file);
-        });
-      });
-    });
-
-    this.server.get(/\/public\/?.*/, restify.serveStatic({
-      directory: path.join(process.cwd(), '/dist'),
+    this.server.get('/', restify.serveStatic({
+      directory: __dirname,
+      default: 'index.html',
     }));
 
-    this.server.get('/', restify.serveStatic({
-      directory: path.join(process.cwd(), '/dist'),
-      default: 'index.html',
+    this.server.get(/\/public\/?.*/, restify.serveStatic({
+      directory: __dirname,
     }));
 
     this.server.get(
